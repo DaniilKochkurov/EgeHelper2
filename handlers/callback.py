89eab4894d1d982953b21b7085e0d0d6f6e2de93
DaniilKochkurov <96654368+DaniilKochkurov@users.noreply.github.com>
2025-11-28 08:56:46 +0300

    callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -974,7 +974,7 @@ async def cmd_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("Пока нет оценок.", reply_markup=InlineKeyboardMarkup(keyboard))
 
-app = ApplicationBuilder().token("8579753971:AAF4Uu9mbCSiUEydp5YY0RLjSVzt3egHIv8").build()
+app = ApplicationBuilder().token("AAF4Uu9mbCSiUEydp5YY0RLjSVzt3egHIv8").build()
 
 app.add_handler(CommandHandler("menu", cmd_menu))
 app.add_handler(CommandHandler("exam", cmd_ekzamen))
@@ -989,3 +989,4 @@ app.add_handler(CommandHandler("feedback", cmd_feedback))
 # app.add_handler(CallbackQueryHandler(handle_callback))
 
 app.run_polling()
+
